137825d10560a69ba7585415b9c99195519a941c
oashtari <oashtari@gmail.com>
2020-03-11 22:00:12 -0700

    completed get to test connection, post to add user

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 const express = require('express');
 
+const shortid = require('shortid');
+
 const server = express();
+
+let users = [
+    {
+        id: shortid.generate(),
+        name: "Mike Johnson",
+        bio: "He dances"
+    },
+    {
+        id: shortid.generate(),
+        name: "Jane Matthewson",
+        bio: "She writes"
+    }
+];
 
 server.listen(4500, () => {
     console.log('listening on 4500')
+})
+
+
+server.use(express.json());
+
+
+// GET TO TEST
+server.get('/', (req, res) => {
+    res.send('you are connected')
+})
+
+//POST 
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    user.id = shortid.generate();
+
+    if (!user.name || !user.bio) {
+        res
+            .status(400)
+            .json({ message: 'Please provide name and bio for the user.' })
+
+    } else if (user.name && user.bio) {
+        users.push(user);
+        res.status(201).json(user);
+
+    } else {
+        res
+            .status(500)
+            .json({ message: 'There was an error while saving the user to the database.' })
+    }
 })
